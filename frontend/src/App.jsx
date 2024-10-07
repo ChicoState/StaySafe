618fb4a9a7e911bdbb684264fea9dce666ef171d
leszakal <lesakal@protonmail.com>
2024-10-06 22:02:16 -0700

    Add basic map to page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,22 +2,31 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import {APIProvider, Map} from '@vis.gl/react-google-maps'
 
 function App() {
   const [count, setCount] = useState(0)
-
+  const GMAPS_API_KEY = globalThis.GMAPS_API_KEY ?? (process.env.GMAPS_API_KEY);
   return (
     <>
     <div>
-      <nav class="top-navbar">
-        <a href="/" class="site-nav-title">StaySafe</a>
+      <nav className="top-navbar">
+        <a href="/" className="site-nav-title">StaySafe</a>
         <a href="/">Home</a>
         <a href="/about">About</a>
       </nav>
     </div>
-    <h1>Map here</h1>
+    <APIProvider apiKey={GMAPS_API_KEY}>
+      <Map
+        style={{width: '100vw', height: '100vh'}}
+        defaultZoom={3}
+        defaultCenter={{lat: 22.54992, lng: 0}}
+        gestureHandling={'greedy'}
+      />
+    </APIProvider>
     </>
   )
 }
+
 
 export default App
