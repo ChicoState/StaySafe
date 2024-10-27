c8f299ef6901981831070bfd615b9130c0abee89
leszakal <lesakal@protonmail.com>
2024-10-27 14:59:50 -0700

    Remove light grey background

--- frontend/src/components/pages/About.jsx
+++ frontend/src/components/pages/About.jsx
@@ -1,12 +1,12 @@
 // src/components/pages/About.jsx
 const About = () => {
-    
-    return (
-        <div style={{ padding: '20px', backgroundColor: 'lightgray' }}>
-            <h2>About Page</h2>
-            <p>StaySafe uses FBI data and Google Maps to make it easy to view the crime statistics of different areas.</p>
-        </div>
-    );
+
+  return (
+    <div style={{ padding: '20px' }}>
+      <h2>About Page</h2>
+      <p>StaySafe uses FBI data and Google Maps to make it easy to view the crime statistics of different areas.</p>
+    </div>
+  );
 };
 
 export default About;
