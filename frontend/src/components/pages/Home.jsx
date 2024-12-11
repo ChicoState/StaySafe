215758ce491c690295ff9771d8963ad15d0d9d97
Feroz Khan <fakhan@csuchico.edu>
2024-12-10 21:11:48 -0800

    refactor(Home): integrate updated ChicoArrests and GoogleMap

--- frontend/src/components/pages/Home.jsx
+++ frontend/src/components/pages/Home.jsx
@@ -1,14 +1,17 @@
-
-
 import GoogleMap from '../map/GoogleMap/GoogleMap'
 // importing chico arrest section.
 import ChicoArrests from '../chico_arrests/ChicoArrests'
+import { useState } from 'react';
 
 const Home = () => {
+  const [addresses, setAddresses] = useState([]);
+  const [loading, setLoading] = useState(true);
+
   return (
     <>
-      <GoogleMap />
-      <ChicoArrests />
+    <ChicoArrests setAddresses={setAddresses} setLoading={setLoading} />
+    <GoogleMap addresses={addresses} loading={loading} />
+      
     </>
   );
 };
